9aa4b58a2e0be0b16fd98b5d39e1541959f276fe
Mohammed CHBANI <mohamedachbani5@gmail.com>
2025-04-10 14:43:05 +0100

    first commit

---
+++ src/main/java/com/websementic/fmp/FinDeModuleProjetApplication.java
@@ -0,0 +1,13 @@
+package com.websementic.fmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinDeModuleProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinDeModuleProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websementic/fmp/FinDeModuleProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websementic.fmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinDeModuleProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
